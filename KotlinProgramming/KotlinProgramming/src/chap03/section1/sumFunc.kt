072cf103e83006909b8c1c13a89d3325643b987a
Sungmin <lion.h2ad@gmail.com>
2023-08-03 14:01:50 +0900

    03 함수와 함수형 프로그래밍 03-1 함수 선언하고 호출하기

---
+++ KotlinProgramming/KotlinProgramming/src/chap03/section1/sumFunc.kt
@@ -0,0 +1,20 @@
+package chap03.section1
+
+/*
+fun sum(a: Int, b : Int): Int {
+    return a+ b
+}
+fun sum(a: Int, b : Int): Int = a+ b
+fun sum(a: Int, b : Int) = a+ b*/
+fun sum(a: Int, b : Int): Int {
+    var sum = a+ b
+    return sum
+}
+
+fun main( ) {
+    val result1 = sum(3,2)
+    val result2 = sum(6,7)
+
+    println(result1)
+    println(result2)
+}
