0efba45ff13853c89a258bd2ea63639726983372
Sungmin <lion.h2ad@gmail.com>
2023-10-05 17:18:50 +0900

    06. 프로퍼티 초기화 06-2 지연 초기화의 위임 - lateinit, lazy

---
+++ KotlinProgramming/KotlinProgramming/src/chap06/section2/Practice.kt
@@ -0,0 +1,10 @@
+package chap06.section2
+
+class Practice {
+    //Lazy Mode
+    /**
+     * SYNCHRONIZED : lock을 사용해 단일 스레드만이 사용하는 것을 보장한다(기본값).
+     * PUBLICATION : 여러 군데에서 호출될 수 있으나 처음 초기화된 후 반환값을 사용한다.
+     * NONE : lock을 사용하지 않기 때문에 빠르지만 다중 스레드가 접근할 수 있다(값의 일관성을 보장할 수 없음).
+     */
+}
