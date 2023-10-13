cd19f0512f867d4f50f99c5df4af9c8ce6838e3f
Sungmin <lion.h2ad@gmail.com>
2023-10-13 16:19:24 +0900

    07. 다양한 클래스와 인터페이스 07-1 추상 클래스와 인터페이스 - abstract, interface

---
+++ KotlinProgramming/KotlinProgramming/src/chap07/section1/InterfacePet.kt
@@ -0,0 +1,33 @@
+package chap07.section1
+
+/**
+ * interface 를 구현 할 때 var 로 하게되면 추상 프로퍼티가 되기 때문에
+ * 상속 받는 곳에서 overriding 을 해주어야 한다.
+ * 함수의 경우에는 구현부를 만들면 비 추상 메서드가 된다.
+ * */
+interface Pet {
+    var category: String // abstract 키워드가 없어도 기본은 추상 프로퍼티
+    val msgTags: String  // val 선언 시 게터의 구현이 가능
+        get() = "I'm your lovely pet!"
+    var species: String // 종을 위한 프로퍼티
+    fun feeding() // 마찬가지로 추상 메서드
+    fun patting() { // 비-추상 메서드: 구현부를 포함하면 비-추상이 되며 기본 구현이 된다.
+        println("Keep patting!") // 구현부
+    }
+}
+
+class Cat(name: String, override var category: String) : Pet, Animal(name) {
+
+    override var species: String = "cat" // 프로퍼티를 오버라이딩해 종을 특정한다.
+    override fun feeding() {
+        println("Feed the cat a tuna can!")
+    }
+}
+
+fun main() {
+    val obj = Cat("Bill", "small")
+    println("Pet Category: ${obj.category}")
+    println("Pet Message Tags: ${obj.msgTags}")
+    obj.feeding() // 구현된 메서드
+    obj.patting() // 기본 메서드
+}
