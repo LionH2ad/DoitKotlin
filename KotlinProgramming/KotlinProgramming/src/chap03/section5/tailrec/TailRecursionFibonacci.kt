29ac008eebfad749c0762c89c847497b79cce01c
Sungmin <lion.h2ad@gmail.com>
2024-01-04 11:55:19 +0900

    피보나치 수열 클래스로 인한 코루틴 실행 불가로 인하여 주석 처리

--- KotlinProgramming/KotlinProgramming/src/chap03/section5/tailrec/TailRecursionFibonacci.kt
+++ KotlinProgramming/KotlinProgramming/src/chap03/section5/tailrec/TailRecursionFibonacci.kt
@@ -1,19 +1,16 @@
 package chap03.section5.tailrec
 
-import java.math.BigInteger
+//import java.math.BigInteger
 
 fun main() {
-    val n = 100
-    val first = BigInteger("0")
-    val second = BigInteger("1")
+//    val n = 100
+//    val first = BigInteger("0")
+//    val second = BigInteger("1")
 
-    println(fibonacci(n, first, second))
-}
+//    println(fibonacci(n, first, second))
+//}
 
 // 꼬리 재귀 함수
-tailrec fun fibonacci(n: Int, a: BigInteger, b: BigInteger): BigInteger {
-    return if (n == 0) a else {
-        println("n-1: ${n - 1}, b: $b, a+b: ${a + b}")
-        fibonacci(n - 1, b, a + b)
-    }
+//tailrec fun fibonacci(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+//    return if (n == 0) a else fibonacci(n - 1, b, a + b)
 }
